a8a77dfc85a990ee59a84cdbda437a0fea2e605a
srz-anandhu <anandhu.rajan@smartrabbitz.com>
2025-04-30 15:25:13 +0530

    Added the functionality to like and dislike a post

--- vite-project/src/components/LikeButton.tsx
+++ vite-project/src/components/LikeButton.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "../supabase-client";
 import { useAuth } from "../contexts/AuthContext";
 
@@ -6,25 +6,106 @@ interface Props {
     postId: number;
 }
 
+interface Vote {
+    id: number
+    post_id: number
+    user_id: string
+    vote: number
+}
+
 const vote = async (voteValue: number, postId: number, userId: string) => {
-    const { error } = await supabase.from("votes").insert({post_id: postId, user_id: userId, vote: voteValue})
-     
+
+    const { data: existingVote } = await supabase
+        .from("votes")
+        .select("*")
+        .eq("post_id", postId)
+        .eq("user_id", userId)
+        .maybeSingle()
+
+    if (existingVote) {
+        // Liked -> 0, Like -> 1
+        if (existingVote.vote === voteValue) {
+            const { error } = await supabase
+                .from("votes")
+                .delete()
+                .eq("id", existingVote.id)
+
+            if (error) throw new Error(error.message)
+        } else {
+            const { error } = await supabase
+                .from("votes")
+                .update({ vote: voteValue })
+                .eq("id", existingVote.id);
+
+            if (error) throw new Error(error.message)
+        }
+    } else {
+
+        const { error } = await supabase
+            .from("votes")
+            .insert({ post_id: postId, user_id: userId, vote: voteValue })
+
+        if (error) throw new Error(error.message)
+    }
+
+}
+
+const fetchVotes = async (postId: number): Promise<Vote[]> => {
+    const { data, error} = await supabase
+    .from("votes")
+    .select("*")
+    .eq("post_id", postId)
+
     if (error) throw new Error(error.message)
+    return data as Vote[]
 }
 
 export const LikeButton = ({ postId }: Props) => {
 
     const { user } = useAuth();
 
+    const queryClient = useQueryClient()
+
+    const { data: votes, isLoading, error } = useQuery<Vote[], Error>({
+        queryKey: ["votes", postId],
+        queryFn: () => fetchVotes(postId),
+        refetchInterval: 5000,
+    })
+
     const { mutate } = useMutation({
         mutationFn: (voteValue: number) => {
 
-        if (!user) throw new Error ("you must be logged in to Vote !");
-        return vote(voteValue, postId, user.id)}})
+            if (!user) throw new Error("you must be logged in to Vote !");
+            return vote(voteValue, postId, user.id)
+        },
+
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["votes", postId]})
+        }
+    })
+
+    if (isLoading) {
+        return <div className="text-yellow-300"> Loading votes...</div>
+    }
+
+    if (error) {
+        return <div className="text-red-500">Error: {error.message}</div>
+    }
+
+    const likes = votes?.filter((v)=>v.vote === 1).length || 0;
+    const disLikes = votes?.filter((v)=>v.vote === -1).length || 0;
+    const userVote = votes?.find((v) => v.user_id === user?.id)?.vote;
+
     return (
-        <div>
-            <button onClick={() => mutate(1)}> 👍 </button>
-            <button onClick={() => mutate(-1)}> 👎 </button>
+        <div className="flex items-center space-x-4 my-4">
+            <button onClick={() => mutate(1)} 
+            className={`px-3 py-1 cursor-pointer rounded transition-colors duration-200 ${
+                userVote === 1 ? "bg-green-500 text-white" : "bg-gray-200 text-black"
+            }`}> 👍 {likes} </button>
+            <button onClick={() => mutate(-1)}
+            className={`px-3 py-1 cursor-pointer rounded transition-colors duration-220 ${
+                userVote === -1 ? "bg-red-500 text-white" : "bg-gray-200 text-black"
+            }`}> 👎 {disLikes} </button>
         </div>
     )
 }
